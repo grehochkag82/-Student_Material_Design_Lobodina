02fafcea9293b3104adebcf4ffe046a85dc3469c
grehochka82 <lobodina0607@mail.ru>
2026-04-15 17:17:11 +0300

    Step 5: Basic UI completed

---
+++ app/src/main/java/com/lobodina/student_material_design/data/DataSource.kt
@@ -0,0 +1,17 @@
+package com.lobodina.student_material_design.data
+
+
+import com.lobodina.student_material_design.R
+
+val students = listOf(
+    Student(R.drawable.st1, R.string.st1, 19, R.string.st1_desc),
+    Student(R.drawable.st2, R.string.st2, 20, R.string.st2_desc),
+    Student(R.drawable.st3, R.string.st3, 18, R.string.st3_desc),
+    Student(R.drawable.st4, R.string.st4, 21, R.string.st4_desc),
+    Student(R.drawable.st5, R.string.st5, 20, R.string.st5_desc),
+    Student(R.drawable.st6, R.string.st6, 19, R.string.st6_desc),
+    Student(R.drawable.st7, R.string.st7, 22, R.string.st7_desc),
+    Student(R.drawable.st8, R.string.st8, 20, R.string.st8_desc),
+    Student(R.drawable.st9, R.string.st9, 21, R.string.st9_desc),
+    Student(R.drawable.st10, R.string.st10, 19, R.string.st10_desc)
+)
